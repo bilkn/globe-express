b0231d397e1445ac5a5a1440034d7d38ff194d30
bilkn <bilkan.konus@hotmail.com>
2021-03-27 16:48:02 +0300

    feat: conditional image preload

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ import cardData from './fixtures/card-slider.json';
 import { Helmet } from 'react-helmet';
 import { detectWebP } from './helpers/detectWebP';
 function App() {
+  const mql = window.matchMedia('(min-width:64em)');
+
   return (
     <>
+      {/* Two Helmet components used, because "react-helmet" doesn't support jsx fragments */}
       <Helmet>
-        {detectWebP() &&
-          cardData.map((data) => (
-            <link rel="preload" href={data.image} as="image" key={data.image} />
-          ))}
+        {cardData.map((data) => (
+          <link
+            rel="preload"
+            href={`${data.background}${detectWebP() ? '.webp' : '.jpg'}`}
+            as="image"
+            key={data.background}
+          />
+        ))}
       </Helmet>
+      {mql.matches && (
+        <Helmet>
+          {cardData.map((data) => (
+            <link
+              rel="preload"
+              href={`${data.image}${detectWebP() ? '.webp' : '.jpg'}`}
+              as="image"
+              key={data.image}
+            />
+          ))}
+        </Helmet>
+      )}
+
       <Home />
     </>
   );
