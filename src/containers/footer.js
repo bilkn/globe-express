fecfafbe181736bf55072b136da70d7fa43060d7
bilkn <bilkan.konus@hotmail.com>
2021-04-04 16:38:51 +0300

    feat: add github and linkedin links

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Footer } from '../components';
+import { Github, Linkedin } from '@styled-icons/bootstrap';
+import 'styled-components/macro';
 
 export function FooterContainer() {
   return (
@@ -9,8 +11,23 @@ export function FooterContainer() {
         <Footer.Link href="https://dribbble.com/Giulio_Cuscito">
           Giulio Cuscito
         </Footer.Link>
-        .
+        <Footer.Span>.</Footer.Span>
       </Footer.Text>
+      <Footer.Wrapper>
+        <Footer.Link
+          href="https://github.com/bilkn"
+          aria-label="GitHub"
+          css="margin-right: 0.7em;"
+        >
+          <Github size="24" css="vertical-align:text-bottom" />
+        </Footer.Link>
+        <Footer.Link
+          href="https://www.linkedin.com/in/bilkankonus/"
+          aria-label="LinkedIn"
+        >
+          <Linkedin size="24" css="vertical-align:text-bottom" />
+        </Footer.Link>
+      </Footer.Wrapper>
     </Footer>
   );
 }
